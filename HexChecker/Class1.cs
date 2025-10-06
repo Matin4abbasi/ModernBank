105c5c57a5588e1804b3ab665a978bf40b004cea
Matin4abbasi <Matina6721@gmail.com>
2025-10-06 20:01:48 +0330

    hex

---
+++ HexChecker/Class1.cs
@@ -0,0 +1,40 @@
+﻿
+namespace HexChecker;
+
+public class HexChecker
+{
+    public bool ValidateHex(string hexCode)
+    {
+        
+        Boolean check = false;
+        if (string.IsNullOrEmpty(hexCode) || hexCode[0] != '#')
+        {
+            return false;
+        }
+        else if (hexCode.Length != 7)
+        {
+            return false;
+        }
+        else
+        {
+            for (int i = 1; i < hexCode.Length; i++)
+            {
+                char word = hexCode[i];
+                if ((word < '0' && word > '9') || (word < 'A' && word > 'F') || (word < 'a' && word > 'f'))
+                {
+                    return false;
+                }
+
+                check = true;
+
+            }
+
+            if (check)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+    }
+}
---
+++ HexCheckerTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿
+namespace HexCheckerTest;
+using HexChecker;
+
+public class UnitTest1
+{
+    private HexChecker checker = new HexChecker();
+    [Fact]
+    public void ValidateHex_ShouldReturnTrue_WhenInputIsValid()    
+    {
+        // Arrange
+        string input = "#CD5C5C";
+
+        // Act
+        bool result = checker.ValidateHex(input);
+
+        // Assert
+        Assert.True(result);
+    }
+
+
+    [Fact]
+    public void ValidateHex_ShouldReturnFalse_WhenInputIsNotValid()    
+    {
+        // Arrange
+        string input = "ku)@";
+
+        // Act
+        bool result = checker.ValidateHex(input);
+
+        // Assert
+        Assert.False(result);
+
+    }
+}
